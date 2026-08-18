03690ea01837e2fc7c5786d9857862770304a484
Juan <37414793+juanmaureira@users.noreply.github.com>
2026-08-18 19:16:47 -0400

    Time: 26 ms (45.63%) | Memory: 80.1 MB (15.25%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let seen = new Map()
-
-    for(let i = 0; i < nums.length; i++){
-        seen.set(nums[i], (seen.get(nums[i]) || 0) + 1)
-        if(seen.get(nums[i]) > 1) return true
-    }
-
-    return false    
+    return !(new Set([...nums]).size === nums.length)
 };
